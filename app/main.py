20a0d1b9ad4576c25a0bf2bc72de91026645f9c2
Murilo Dionizio <murilomelodionizio@gmail.com>
2026-05-11 16:14:14 -0300

    Rota inicial com usuario opcinal

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends, Request
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from app.auth import get_usuario_opcional
 from app.controllers import auth_controller
 
 app = FastAPI(title="Sistema MVC")
@@ -11,3 +12,21 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(auth_controller.router)
+
+@app.get("/")
+def home(
+    request: Request, 
+    usuario = Depends(get_usuario_opcional)
+    ):
+
+    if usuario is None:
+        return templates.TemplateResponse(
+            request,
+            "index.html",
+            {"request": request}
+        )
+    return templates.TemplateResponse(
+            request,
+            "home.html",
+            {"request": request, "usuario": usuario}
+        )
